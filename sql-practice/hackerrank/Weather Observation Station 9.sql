047553cf9339c4c51f2217e9ea37b884210bbb0c
Swarnali Mollick <76970926+itsmeSwarnali@users.noreply.github.com>
2026-05-31 23:14:47 +0200

    Add SQL query for cities not starting with vowels
    Query to select distinct city names from STATION that do not start with vowels.

--- sql-practice/hackerrank/Weather Observation Station 9.sql
+++ sql-practice/hackerrank/Weather Observation Station 9.sql
@@ -1,1 +1,6 @@
 
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+
+  
+select distinct city from station where upper(left(city,1)) not in ('A', 'E', 'I', 'O', 'U');
