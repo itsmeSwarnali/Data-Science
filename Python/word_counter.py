6b460bc391ece970ffddaeb0158a2b776c930154
Swarnali Mollick <76970926+itsmeSwarnali@users.noreply.github.com>
2026-06-04 13:14:09 +0200

    Add word length counter function in word_counter.py

---
+++ Python/word_counter.py
@@ -0,0 +1,10 @@
+def get_length(word):
+    split_word = split_word = word.split(",")
+    return split_word
+
+
+
+word = input("Enter a sentence: ")
+
+result = get_length(word)
+print("Length: ", len(result))
